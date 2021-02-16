3f558fc3a68b1846a4ae0ce2665958156b4f1f91
hangueira <hks713@naver.com>
2021-02-16 18:09:38 +0900

    localStorage save & load ADD

--- toDo.js
+++ toDo.js
@@ -4,30 +4,51 @@ const toDoList = document.querySelector('.toDoList');
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement('li');
   const delBtn = document.createElement('button');
-  delBtn.innerText = '삭제';
   const span = document.createElement('span');
+  const newId = toDos.length + 1;
+  delBtn.innerText = '삭제';
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text : text,
+    id : newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
-  console.log(currentValue);
   paintToDo(currentValue);
   toDoInput.value = null;
 }
 
-function init() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if(toDos !== null) {
-
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null){
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    });
   }
+}
+
+function init() {
+  // localStorage 에 있는것을 가져오는것으로 변경
+  loadToDos();
   toDoForm.addEventListener('submit', handleSubmit);
 }
 
